8bfae283e2faf60529fc2981e559991dc46672a6
Jason Neal <jason.neal@astro.up.pt>
2016-11-14 08:13:34 +0000

    add alternate chisquared

--- alpha_detection_limit.py
+++ alpha_detection_limit.py
@@ -40,6 +40,18 @@ def chi_squared(observed, expected, error=None):
         # When divided by exted the result is identical to scipy
     return chisqr
 
+@jit
+def alternate_chi_squared(observed, expected, error=None):
+    """Calculate chi squared.
+    Same result as as scipy.stats.chisquare
+    """
+    if error:
+        chisqr = np.sum((observed-expected)**2 / observed)
+    else:
+        # chisqr = np.sum((observed-expected)**2)
+        chisqr = np.sum((observed-expected)**2 / expected)
+        # When divided by exted the result is identical to scipy
+    return chisqr
 
 @jit
 def add_noise(flux, SNR):
