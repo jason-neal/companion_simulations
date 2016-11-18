529cdcca14661578e3b9255d4661eec6708f5609
Jason Neal <jason.neal@astro.up.pt>
2016-11-18 15:38:02 +0000

    Fix up chisqr function

--- alpha_detection_limit_multiprocess.py
+++ alpha_detection_limit_multiprocess.py
@@ -49,24 +49,10 @@ def chi_squared(observed, expected, error=None):
     Same result as as scipy.stats.chisquare
     """
     if np.any(error):
-        chisqr = np.sum((observed-expected)**2 / error**2)
+        chisqr = np.sum((observed - expected) ** 2 / (error ** 2))
     else:
         # chisqr = np.sum((observed-expected)**2)
-        chisqr = np.sum((observed-expected)**2 / expected)
-        # When divided by exted the result is identical to scipy
-    return chisqr
-
-
-@jit
-def alternate_chi_squared(observed, expected, error=None):
-    """Calculate chi squared.
-    Same result as as scipy.stats.chisquare
-    """
-    if error:
-        chisqr = np.sum((observed-expected)**2 / observed)
-    else:
-        # chisqr = np.sum((observed-expected)**2)
-        chisqr = np.sum((observed-expected)**2 / expected)
+        chisqr = np.sum((observed - expected)**2 / expected)
         # When divided by exted the result is identical to scipy
     return chisqr
 
