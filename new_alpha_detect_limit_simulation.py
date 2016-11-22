5708a883e6e9622815f7c4e4a7fa3cdefdf7da8e
Jason Neal <jason.neal@astro.up.pt>
2016-11-22 09:06:59 +0000

    Function to call chi_square on spectrum.

--- new_alpha_detect_limit_simulation.py
+++ new_alpha_detect_limit_simulation.py
@@ -2,3 +2,25 @@
 # New Version of alpha_detection using Parallel and methodolgy from grid_chisquare.
 
  
+
+def spectrum_chisqr(spectrum_1, spectrum_2, error=None):
+    """ Chi squared for specturm objects. """
+    # Spectrum wrapper for chissquare
+    # make sure xaxis is the Same
+    # if len(spectrum_1) == len(spectrum_2):
+    if np.all(spectrum_1.xaxis == spectrum_2.xaxis):
+        # print("xaxis are equal")
+        c2 = chi_squared(spectrum_1.flux, spectrum_2.flux, error=error)
+        # return chi_squared(spectrum_1.flux, spectrum_2.flux, error=None)
+        # print("chisqrayured value", c2)
+        # plot_spectrum(spectrum_1, label="obs", show=False)
+        # plot_spectrum(spectrum_2, label="evauated")
+        if np.isnan(c2):
+            print(" Nan chisqr")
+            # print(spectrum_1.xaxis, spectrum_1.flux, spectrum_2.xaxis, spectrum_2.flux)
+        return c2
+    else:
+
+        #print(len(spectrum_1), len(spectrum_2))
+        raise Exception("TODO: make xaxis equal in chisquare of spectrum")
+
