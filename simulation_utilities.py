61d85a189ddaf37314b85332a25ef3b1f9b001c1
Jason Neal <jason.neal@astro.up.pt>
2016-11-23 12:00:12 +0000

    module to host many functions that are comonly used

---
+++ simulation_utilities.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# Simulation Utilities
+
+# File to contain function necessary for the chi_square simulations
+from __future__ import division, print_function
+import numpy as np
