cf5fdaee393d38bc468f992beb63cf89904bde71
Jason Neal <jason.neal@astro.up.pt>
2017-03-29 07:49:54 +0100

    double doppler shifted model

--- models/alpha_model.py
+++ models/alpha_model.py
@@ -77,3 +77,35 @@ def alpha_model2(alpha, rv, host, companion, limits, new_x=None):
         print("Combined spectrum is all Nans after wav_select")
 
     return combined
+
+
+def double_shifted_alpha_model(alpha, rv1, rv2, host, companion, limits, new_x=None):
+    """Entangled spectrum model.
+
+    inputs:
+    spectrum_1
+    spectrum_2
+    alpha
+    rv - rv offset of spec2
+    xrange = location of points to return for spectrum. e.g. observation.xaxis.
+    should find better name.
+
+    returns:
+    Spectrum object
+
+    """
+    # this copy solved my nan issue.
+    companion = copy.copy(companion)
+    host = copy.copy(host)
+
+    host.doppler_shift(rv1)
+    companion.doppler_shift(rv2)
+
+    combined = combine_spectra(host, companion, alpha)
+
+    if np.any(new_x):
+        combined.spline_interpolate_to(new_x)
+    combined.wav_select(limits[0], limits[1])
+    # observation.wav_select(2100, 2200)
+
+    return combined
