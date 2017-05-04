a78dbff452a4b5661bda369e5fcbccc89d71717f
Jason Neal <jason.neal@astro.up.pt>
2017-05-04 16:25:04 +0100

    Update parsing of parameterfile.
    Lists and extra line comments.

--- utilities/param_file.py
+++ utilities/param_file.py
@@ -1,21 +1,23 @@
 import os
 import logging
+from typing import Dict, Union, List
 
 
-def parse_paramfile(param_file, path=None):
+def parse_paramfile(param_file: str, path: str=None) -> Dict[str, Union[str, float, List[float]]]:
     """Extract orbit and stellar parameters from parameter file.
 
-    parameters
+    Parameters
     ----------
     param_file: str
         Filename of parameter file.
     path: str [optional]
         Path to directory of filename.
 
-    Reuturns
+    Returns
     --------
+    parameters: dict
+        Paramemters as a {param: value} dictionary.
     """
-
     if path is not None:
         param_file = os.path.join(path, param_file)
     parameters = dict()
@@ -27,11 +29,31 @@ def parse_paramfile(param_file, path=None):
             if line.startswith("#"):
                 pass
             else:
+                if '#' in line:   # Remove comment from end of line
+                    line = line.split("#")[0]
+                if line.endswith("="):
+                    logging.warning(("Parameter missing value in {}.\nLine = {line}."
+                                    " Value set to None.").format(param_file, line))
+                    line = line + " None"   # Add None value when parameter is missing
                 par, val = line.lower().split('=')
-                try:
-                    # Turn parameters to floats if possible.
-                    parameters[par.strip()] = float(val.strip())
-                except ValueError:
-                    parameters[par.strip()] = val.strip()
+                par, val = par.strip(), val.strip()
+                if (val.startswith("[") and val.endswith("]")) or ("," in val):  # Val is a list
+                    parameters[par] = parse_list_string(val)
+                else:
+                    try:
+                        parameters[par] = float(val)  # Turn parameters to floats if possible.
+                    except ValueError:
+                        parameters[par] = val
 
     return parameters
+
+
+def parse_list_string(string: str) -> List[Union[str, float]]:
+    """Parse list of floats out of a string."""
+    string = string.replace("[", "").replace("]", "").strip()
+    list_str = string.split(",")
+    try:
+        return [float(val) for val in list_str]
+    except ValueError as e:
+        # Can't turn into floats.
+        return [val.strip() for val in list_str]
