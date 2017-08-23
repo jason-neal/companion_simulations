5156987bb881b29b4d0e625ebe50d430d520bd64
Jason Neal <jason.neal@astro.up.pt>
2017-08-23 12:57:33 +0100

    Load database and try plot some things.

--- bin/analysis_chi2_db.py
+++ bin/analysis_chi2_db.py
@@ -5,6 +5,8 @@ import sys
 
 import pandas as pd
 from sqlalchemy import create_engine
+import sqlalchemy as sa
+import matplotlib.pyplot as plt
 
 
 def _parser():
@@ -13,7 +15,7 @@ def _parser():
     :returns: the args
     """
     parser = argparse.ArgumentParser(description='Chisquare analysis.')
-    parser.add_argument('database', help='database')
+    parser.add_argument('database', help='Database name.')
     # parser.add_argument('-s', '--suffix', help='Suffix to add to database name.')
     # parser.add_argument('-v', '--verbose', help='Turn on Verbose.', action="store_true")
 
@@ -22,11 +24,68 @@ def _parser():
 
 def main(database):
 
-    csv_database = create_engine('sqlite:///{}.db'.format(database), echo=True)
+    engine = create_engine('sqlite:///{}'.format(database), echo=True)
+    table_names = engine.table_names()
+    print("Table names in database =", engine.table_names())
+    if len(table_names) == 1:
+        tb_name = table_names[0]
+    else:
+        raise ValueError("Database has two many tables {}".format(table_names))
+    df = pd.read_sql_query('SELECT alpha, chi2 FROM {0}'.format(tb_name), engine)
 
-    df = pd.read_sql_query('SELECT * FROM table', csv_database)
+    fig, ax = plt.subplots()
+    ax.scatter(df["alpha"], df["chi2"], s=3, alpha=0.5)
 
-    df.plot()
+    ax.set_xlabel('alpha', fontsize=15)
+    ax.set_ylabel('chi2', fontsize=15)
+    ax.set_title('alpha and Chi2.')
+
+    ax.grid(True)
+    fig.tight_layout()
+
+    plt.show()
+
+    alpha_rv_plot(engine, tb_name)
+    alpha_rv_contour(engine, tb_name)
+
+
+def alpha_rv_plot(engine, tb_name=None):
+    # df = pd.read_sql(sa.text('SELECT alpha, rvs, chi2, teff_2 FROM table WHERE teff_1=:teff1, logg_1=:logg1, feh_1=:feh1'), engine, params={'teff1': 5200, 'logg1': 4.5, 'feh1': 0.0})
+    #df = pd.read_sql(sa.text('SELECT alpha, rvs, chi2, teff_2 FROM table WHERE teff_1=:teff_1'), engine, params={'teff_1': 5200})
+    #df = pd.read_sql(sa.text('SELECT alpha, rvs, chi2, teff_2 FROM table WHERE teff_1=5200 and logg_1=4.5 and feh_1=0.0'), engine)
+    df = pd.read_sql(sa.text('SELECT alpha, rv, chi2, teff_2 FROM {0}'.format(tb_name)), engine)
+    #df = pd.read_sql_query('SELECT alpha  FROM table', engine)
+
+    fig, ax = plt.subplots()
+    ax.scatter(df["rv"], df["chi2"], c=df["alpha"], s=df["teff_2"] / 50, alpha=0.5)
+
+    ax.set_xlabel('rv offset', fontsize=15)
+    ax.set_ylabel('chi2', fontsize=15)
+    ax.set_title('alpha (color) and companion temperature (size=Temp/50).')
+
+    ax.grid(True)
+    fig.tight_layout()
+
+    plt.show()
+
+
+def alpha_rv_contour(engine, tb_name=None):
+    # df = pd.read_sql(sa.text('SELECT alpha, rvs, chi2, teff_2 FROM table WHERE teff_1=:teff1, logg_1=:logg1, feh_1=:feh1'), engine, params={'teff1': 5200, 'logg1': 4.5, 'feh1': 0.0})
+    #df = pd.read_sql(sa.text('SELECT alpha, rvs, chi2, teff_2 FROM table WHERE teff_1=:teff_1'), engine, params={'teff_1': 5200})
+    #df = pd.read_sql(sa.text('SELECT alpha, rvs, chi2, teff_2 FROM table WHERE teff_1=5200 and logg_1=4.5 and feh_1=0.0'), engine)
+    df = pd.read_sql(sa.text('SELECT alpha, rv, chi2, teff_2 FROM {0}'.format(tb_name)), engine)
+    #df = pd.read_sql_query('SELECT alpha  FROM table', engine)
+
+    fig, ax = plt.subplots()
+    ax.contourf(df["rv"], df["chi2"], c=df["alpha"], alpha=0.5)
+
+    ax.set_xlabel('rv offset', fontsize=15)
+    ax.set_ylabel('chi2', fontsize=15)
+    ax.set_title('alpha (color)')
+
+    ax.grid(True)
+    fig.tight_layout()
+
     plt.show()
 
 
