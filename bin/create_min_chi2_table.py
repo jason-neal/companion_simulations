3a74e69ab0021c8e70e7a43eedf391764c8fadb0
Jason Neal <jason.neal@astro.up.pt>
2017-08-31 17:08:40 +0100

    Scatter matrix plot of min chi2 values.

--- bin/create_min_chi2_table.py
+++ bin/create_min_chi2_table.py
@@ -7,6 +7,7 @@ import os
 import matplotlib.pyplot as plt
 import pandas as pd
 import sqlalchemy as sa
+from pandas.plotting import scatter_matrix
 
 from bin.analysis_iam_chi2 import decompose_database_name, get_host_params
 from utilities.phoenix_utils import closest_model_params
@@ -55,6 +56,40 @@ def main(star, obs_num, chip, echo=False):
     return save_name
 
 
+def scatter_plots(star, name):
+    """Load minimum chi2 table and make scatter plots across chips."""
+    df = pd.read_table(name, sep="\t")
+    print(df.columns)
+    print(df.dtypes)
+
+    df.loc[:, "chip"] = df.loc[:, "chip"].astype(int)
+    print(df.dtypes)
+    print(df)
+    fig, axes = plt.subplots(5, 2)
+    print(axes)
+    print(axes[0, 0])
+    # df.plot(ax=axes[0, 0]).scatter("chip", "chi2")
+    # df.plot(ax=axes[0, 1]).scatter("chip", "teff_2", c="chi2", colorbar=True)
+    # df.plot(ax=axes[1, 0]).scatter("chip", "alpha", c="chi2", colorbar=True)
+    # df.plot(ax=axes[1, 1]).scatter("chip", "rv", c="chi2", colorbar=True)
+    # df.plot(ax=axes[2, 0]).scatter("chip", "gamma", c="chi2", colorbar=True)
+    # df.plot(ax=axes[2, 1]).scatter("gamma", "alpha", c="chi2", colorbar=True)
+    # df.plot(ax=axes[3, 0]).scatter("teff_2", "rv", c="chi2", colorbar=True)
+    # df.plot(ax=axes[3, 1]).scatter("teff_2", "gamma", c="chi2", colorbar=True)
+    # df.plot(ax=axes[4, 0]).scatter("teff_2", "alpha", c="chi2", colorbar=True)
+    # df.plot(ax=axes[4, 1]).scatter("rv", "alpha", c="chi2", colorbar=True)
+    subdf = df.loc[:, ["chip", "teff_2", "alpha", "rv", "gamma", "chi2"]]  # "logg_2", "feh_2"
+
+    scatter_matrix(subdf, alpha=1, figsize=(12, 12), diagonal='hist')
+    plt.suptitle("{} Observation/chip variations".format(star))
+
+    path, fname = os.path.split(name)
+    figname = os.path.join(path, "plots", "{}_scatter.pdf".format(fname.split(".")[0]))
+    plt.savefig(figname)
+
+    figname = os.path.join(path, "plots", "{}_scatter.png".format(fname.split(".")[0]))
+    plt.savefig(figname)
+
 
 if __name__ == "__main__":
     stars = ["HD30501"]
@@ -71,3 +106,4 @@ if __name__ == "__main__":
                     print(e)
                     print("Table creation failed for {0}-{1}_{2}".format(star, obs_num, chip))
                     continue
+        scatter_plots(star, save_name)
