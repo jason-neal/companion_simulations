e8c8464d36e91c9a8d61db0531a2e73dcdee88b7
Jason Neal <jason.neal@astro.up.pt>
2017-09-22 14:18:07 +0100

    Add a test for check_inputs.

---
+++ utilities/tests/test_simulation_utils.py
@@ -0,0 +1,14 @@
+from utilities.simulation_utilities import check_inputs
+
+import pytest
+import numpy as np
+
+
+@pytest.mark.parametrize("input,expected", [
+    (None, np.ndarray([0])),
+    ([0], np.array([0])),
+    (1, np.array([1])),
+    (range(5), np.array([0,1,2,3,4]))
+])
+def test_check_inputs(input, expected):
+    assert np.allclose(check_inputs(input), expected)
