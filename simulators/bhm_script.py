173122226883975e24f79ce4928149c9dd72c939
Jason Neal <jason.neal@astro.up.pt>
2017-09-22 14:12:04 +0100

    remove masking function

--- simulators/bhm_script.py
+++ simulators/bhm_script.py
@@ -65,16 +65,6 @@ def save_pd_cvs(name, data):
     return 0
 
 from utilities.masking import get_maskinfo
- # def get_maskinfo(star, obs_num, chip):
- #    with open("/home/jneal/.handy_spectra/detector_masks.json", "r") as f:
- #        mask_data = json.load(f)
- #    try:
- #        this_mask = mask_data[star][obs_num][str(chip)]
- #        print(this_mask)
- #        return this_mask
- #    except KeyError:
- #        print("No Masking data present for {0}-{1}_{2}".format(star, obs_num, chip))
- #        return []
 
 
 def deconstruct_array(array, values):
