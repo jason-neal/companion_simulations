2078d08477e7767f780b4c683704c63de5c23b77
Jason Neal <jason.neal@astro.up.pt>
2017-10-06 17:19:22 +0100

    Reading in 4 csv at onces to being coadding.

--- bin/coadd_detector_chi2.py
+++ bin/coadd_detector_chi2.py
@@ -5,15 +5,15 @@
 Create Table of minimum Chi_2 values and save to a table.
 """
 import argparse
+import glob
 import os
 
 import corner
 import matplotlib.pyplot as plt
-#import pandas as pd
+import pandas as pd
 import sqlalchemy as sa
 #from joblib import Parallel, delayed
 #from pandas.plotting import scatter_matrix
-
 import simulators
 from bin.analysis_iam_chi2 import decompose_database_name
 from utilities.param_file import get_host_params
@@ -41,7 +41,56 @@ def main(star, obs_num, suffix):
 
     print("databases", databases)
 
+    #sql_join and add to databaseand
+    patterns = [os.path.join(
+            simulators.paths["output_dir"], star,
+            "{0}-{1}_{2}_iam_chisqr_results{3}*.csv".format(star, obs_num, chip, suffix))
+        for chip in range(1,5)]
+    # print("first Patterns", patterns)
+    # print((sum(1 for _ in glob.iglob(pattern)) for pattern in patterns))
+    if (sum(1 for _ in glob.iglob(patterns[0]))) == 0:
+        patterns = [os.path.join(
+                simulators.paths["output_dir"], star, "processed_csv",
+                "{0}-{1}_{2}_iam_chisqr_results{3}*.csv".format(star, obs_num, chip, suffix))
+            for chip in range(1,5)]
 
+    print("new Patterns", patterns)
+    if (sum(1 for _ in glob.iglob(pattern)) for pattern in patterns) == 0:
+        print("pattern lengths", [sum(1 for _ in glob.iglob(pattern)) for pattern in patterns])
+        raise ValueError("Issue with patterns finding files")
+
+    #load all 4 detectors cvs at same timeself.
+    #print("end Patterns", patterns)
+    # Try from csv files
+
+    print("pattern lengths", [sum(1 for _ in glob.iglob(pattern)) for pattern in patterns])
+
+
+    # get list of patterns. and sort in order for loading in.
+    detector_files = [sorted(glob.glob(pattern)) for pattern in patterns]
+    print(detector_files, len(detector_files))
+
+
+    for i, files  in enumerate(zip(*detector_files)):
+        print(i)
+        print(files)
+
+        pd_iter1 = pd.read_csv(files[0], iterator=True, chunksize=10)
+        pd_iter2 = pd.read_csv(files[1], iterator=True, chunksize=10)
+        pd_iter3 = pd.read_csv(files[2], iterator=True, chunksize=10)
+        pd_iter4 = pd.read_csv(files[3], iterator=True, chunksize=10)
+
+        print(pd_iter4)
+
+        df_1 = pd_iter1.get_chunk()
+        df_2 = pd_iter2.get_chunk()
+        df_3 = pd_iter3.get_chunk()
+        df_4 = pd_iter4.get_chunk()
+
+        print("df_2 head", df_1.head())
+        print("df_2 head", df_2.head())
+        print("df_3 head", df_3.head())
+        print("df_4 head", df_4.head())
 
 if __name__ == "__main__":
     args = _parser()
