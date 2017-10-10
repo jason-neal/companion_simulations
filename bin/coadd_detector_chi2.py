968178b2113bd8b8fe29b899b3ae67859262dcd5
Jason Neal <jason.neal@astro.up.pt>
2017-10-10 13:24:24 +0100

    Coadd the chisquare values.

--- bin/coadd_detector_chi2.py
+++ bin/coadd_detector_chi2.py
@@ -12,13 +12,11 @@ import corner
 import matplotlib.pyplot as plt
 import pandas as pd
 import sqlalchemy as sa
-#from joblib import Parallel, delayed
-#from pandas.plotting import scatter_matrix
+
 import simulators
 from bin.analysis_iam_chi2 import decompose_database_name
 from utilities.param_file import get_host_params
 from utilities.phoenix_utils import closest_model_params
-#from utilities.scatter_corner import scatter_corner
 
 
 def _parser():
@@ -26,28 +24,28 @@ def _parser():
 
     :returns: the args
     """
-    parser = argparse.ArgumentParser(description='Co-added Chi-squared db.')
-    parser.add_argument('-s', '--stars', help='Star names', nargs="+", default=None)
-    parser.add_argument("-o", "--obsnum", help="Observation number", nargs="+", default=None)
-    parser.add_argument('--suffix', help='Suffix to add to the file names.', default="")
+    parser = argparse.ArgumentParser(description='Create Co-added Chi-squared db.')
+    parser.add_argument('-s', '--stars', nargs="+", default=None,
+                        help='Star names')
+    parser.add_argument("-o", "--obsnum", nargs="+", default=None,
+                        help="Observation number")
+    parser.add_argument('--suffix', default="",
+                        help='Suffix to add to the file names.')
+    parser.add_argument('-v', '--verbose', action="store_true",
+                        help='Enable verbose.')
+    parser.add_argument('-r', '--replace', action="store_true",
+                        help='Overwrite the databse if already exists.')
+    parser.add_argument('-c', '--chunksize', default=1000, type=int,
+                        help='Chinksize for reading in csv files.')
     return parser.parse_args()
 
 
-def main(star, obs_num, suffix):
-    databases = ["", "", "", ""]
-    for chip in range(1, 5):
-        databases[chip-1] = os.path.join(simulators.paths["output_dir"], star,
-           "{0}-{1}_{2}_iam_chisqr_results{3}.db".format(star, obs_num, chip, suffix))
-
-    print("databases", databases)
-
-    #sql_join and add to databaseand
+def main(star, obs_num, suffix, replace=False, verbose=True, chunksize=1000):
     patterns = [os.path.join(
             simulators.paths["output_dir"], star,
             "{0}-{1}_{2}_iam_chisqr_results{3}*.csv".format(star, obs_num, chip, suffix))
         for chip in range(1,5)]
-    # print("first Patterns", patterns)
-    # print((sum(1 for _ in glob.iglob(pattern)) for pattern in patterns))
+
     if (sum(1 for _ in glob.iglob(patterns[0]))) == 0:
         patterns = [os.path.join(
                 simulators.paths["output_dir"], star, "processed_csv",
@@ -55,50 +53,96 @@ def main(star, obs_num, suffix):
             for chip in range(1,5)]
 
     print("new Patterns", patterns)
-    if (sum(1 for _ in glob.iglob(pattern)) for pattern in patterns) == 0:
+    if sum(sum(1 for _ in glob.iglob(pattern)) for pattern in patterns) == 0:
+        raise ValueError("Issue with patterns finding for {0} obs {1}".format(star, obs_num))
+
+    # Start up database
+    coadd_database = os.path.join(
+            simulators.paths["output_dir"], star,
+            "{0}-{1}_coadd_iam_chisqr_results{2}.db".format(star, obs_num, suffix))
+
+    print("Replace", replace)
+    print("os.path.isfile(coadd_database)", os.path.isfile(coadd_database))
+    if os.path.isfile(coadd_database):
+       if replace:
+           os.remove(coadd_database)
+       else:
+           raise IOError("The database file {0} already exists."
+                " Add the switch -r to replace the old database file.".format(
+                coadd_database))
+
+    database_name = 'sqlite:///{0}'.format(coadd_database)
+    engine = sa.create_engine(database_name)
+    if verbose:
+        print("csv_database =", engine, type(engine))
+
         print("pattern lengths", [sum(1 for _ in glob.iglob(pattern)) for pattern in patterns])
-        raise ValueError("Issue with patterns finding files")
-
-    #load all 4 detectors cvs at same timeself.
-    #print("end Patterns", patterns)
-    # Try from csv files
-
-    print("pattern lengths", [sum(1 for _ in glob.iglob(pattern)) for pattern in patterns])
-
 
     # get list of patterns. and sort in order for loading in.
     detector_files = [sorted(glob.glob(pattern)) for pattern in patterns]
-    print(detector_files, len(detector_files))
 
+    i = 0
+    j = 1
+    for num, files  in enumerate(zip(*detector_files)):
+        assert len(files) == 4
+        f_0 = files[0]
 
-    for i, files  in enumerate(zip(*detector_files)):
-        print(i)
-        print(files)
+        if "[" in f_0:
+            n = f_0.split("[")[-1]
+            n = n.split("]")[0]
+            assert all(n in f for f in files)   # All have this same host
+            teff, logg, feh = n.split("_")
+            if verbose:
+                print("host params", teff, logg, feh)
+            host_flag = True
+        else:
+            host_flag = False
 
-        pd_iter1 = pd.read_csv(files[0], iterator=True, chunksize=10)
-        pd_iter2 = pd.read_csv(files[1], iterator=True, chunksize=10)
-        pd_iter3 = pd.read_csv(files[2], iterator=True, chunksize=10)
-        pd_iter4 = pd.read_csv(files[3], iterator=True, chunksize=10)
+        # Initalize iterators:
+        iterators =  [pd.read_csv(f, iterator=True, chunksize=chunksize) for f in files]
 
-        print(pd_iter4)
+        while True:
+            try:
+                chunks = [pd_iter.get_chunk() for pd_iter in iterators]
+                assert all([len(chunks[k]) == len(chunks[l]) for k, l in ((0,1), (1,2), (2,3))])
+            except StopIteration:
+                break
 
-        df_1 = pd_iter1.get_chunk()
-        df_2 = pd_iter2.get_chunk()
-        df_3 = pd_iter3.get_chunk()
-        df_4 = pd_iter4.get_chunk()
+            joint_12 = pd.merge(chunks[0], chunks[1], how="outer", on=['teff_2', 'logg_2', 'feh_2', 'rv', 'gamma'], suffixes=["_1", "_2"])
+            joint_34 = pd.merge(chunks[2], chunks[3],  how="outer", on=['teff_2', 'logg_2', 'feh_2', 'rv', 'gamma'], suffixes=["_3", "_4"])
+            pd_joint = pd.merge(joint_12, joint_34,  how="outer", on=['teff_2', 'logg_2', 'feh_2', 'rv', 'gamma'])
 
-        print("df_2 head", df_1.head())
-        print("df_2 head", df_2.head())
-        print("df_3 head", df_3.head())
-        print("df_4 head", df_4.head())
+            # co-adding chisquare values across detectors
+            pd_joint["coadd_chi2"] = pd_joint["chi2_1"] + pd_joint["chi2_2"] + pd_joint["chi2_3"] + pd_joint["chi2_4"]
+
+            assert not pd_joint.isnull().values.any(), "There are nans in the joint dataframe!!!"
+
+            # Adding host parameters
+            pd_joint["teff_1"] = teff
+            pd_joint["logg_1"] = logg
+            pd_joint["feh_1"] = feh
+            pd_joint = pd_joint.rename(columns={c: c.replace(' ', '').lower() for c in pd_joint.columns})
+            pd_joint.index += j
+
+            i += 1
+            pd_joint.to_sql('chi2_table', engine, if_exists='append')
+            j = pd_joint.index[-1] + 1
+            if verbose:
+                print("indicies = ", i, j)
+
+        if verbose:
+            print("Reached end of part =", num)
+
+    if verbose:
+        print("Completed coadd db creation")
+
 
 if __name__ == "__main__":
-    args = _parser()
-    stars = args.stars
-    obsnums = args.obsnum
-  #  if stars is None:
-   #     stars = ["HD30501", "HD211847", "HD4747"]
+    args = vars(_parser())
+    stars = args.pop('stars')
+    obsnums = args.pop('obsnum')
+    opts = {k: args[k] for k in args}
 
     assert len(stars) == len(obsnums), "Number of stars and obsnums need to be the same number."
     for star, obs in zip(stars, obsnums):
-        main(star, obs, args.suffix)
+        main(star, obs, **opts)
