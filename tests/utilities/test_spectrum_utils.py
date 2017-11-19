84f20ac9b0445771d5a4f4327a3e29f8ec9b1cb8
Jason Neal <jason.neal@astro.up.pt>
2017-11-19 06:57:07 +0000

    test loading some spectra

--- tests/utilities/test_spectrum_utils.py
+++ tests/utilities/test_spectrum_utils.py
@@ -1,29 +1,35 @@
+import os
+
+import numpy as np
 import pytest
 from spectrum_overload import Spectrum
 
 from mingle.utilities.spectrum_utils import load_spectrum, select_observation
 
 
-@pytest.mark.xfail
-def test_load_spectrum():
-    load_spectrum()
-    assert False
+@pytest.mark.parametrize("fname", ["HD30501-1-mixavg-tellcorr_1.fits", "HD30501-1-mixavg-h2otellcorr_1.fits"])
+def test_load_spectrum(fname):
+    fname = os.path.join("tests", "testdata", "handy_spectra", fname)
+    results = load_spectrum(fname)
     assert isinstance(results, Spectrum)
+    assert results.header["OBJECT"] == "HD30501"
+    assert np.all(results.xaxis > 2110)  # nm
+    assert np.all(results.xaxis < 2130)  # nm
+    assert np.all(results.flux < 2)
+    assert np.all(results.flux >= 0)
 
 
-@pytest.mark.xfail
-def test_load_spectrum_with_failure():
-    assert False
+def test_load_no_filename_fits():
+    """Not a valid file."""
+    with pytest.raises(ValueError):
+        load_spectrum("")
 
-
-@pytest.mark.xfail
-def test_select_observation():
-    assert False
 
 @pytest.mark.parametrize("chip", [0, None, 5, 42])
 def test_select_observation_with_bad_chip(chip):
     with pytest.raises(ValueError):
         select_observation("HD30501", "1", chip)
+
 
 @pytest.mark.xfail
 def test_spectrum_plotter(spectra, label=None, show=False):
@@ -35,4 +41,3 @@ def test_spectrum_plotter(spectra, label=None, show=False):
 def test_plot_spectra(obs, model):
     """Plot two spectra."""
     assert False
-
