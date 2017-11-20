a7f2671d462078014b5ecc09d514d43613777f4e
Jason Neal <jason.neal@astro.up.pt>
2017-11-20 17:09:49 +0000

    test bhm pars, faling

--- tests/simulators/test_bhm_script.py
+++ tests/simulators/test_bhm_script.py
@@ -7,3 +7,9 @@ from simulators.bhm_script import (bhm_helper_function, get_model_pars,
 @pytest.mark.xfail()
 def test_save_pd_cvs(tmpdir):
     assert 0
+
+
+def test_get_model_pars():
+    pars = get_model_pars({"temp": 5200, "logg": 4.5, "fe_h": 0.0})
+    assert pars == False
+    assert False
