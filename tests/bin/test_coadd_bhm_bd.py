9382ff17ca4d416f5c36cf0879960aab9eff9d70
Jason Neal <jason.neal@astro.up.pt>
2017-11-24 11:58:44 +0000

    Tests for db creator script parsers.

---
+++ tests/bin/test_coadd_bhm_bd.py
@@ -0,0 +1,26 @@
+from bin.coadd_bhm_db import parse_args
+
+def test_coadd_bhm_db_parser_defaults():
+    parsed = parse_args(["HDdefault", "0", ])
+    assert parsed.star == "HDdefault"
+    assert parsed.obsnum == "0"
+    assert parsed.suffix is ""
+    assert parsed.chunksize == 1000
+    assert isinstance(parsed.chunksize, int)
+    assert parsed.replace is False
+    assert parsed.verbose is False
+    assert parsed.move is False
+
+
+def test_coadd_bhm_db_parser():
+    parsed = parse_args(["HDswitches", "1a", "--suffix", "_test",
+                         "-v", "-r", "-c", "50000", "-m"])
+    assert parsed.star == "HDswitches"
+    assert parsed.obsnum == "1a"
+    assert parsed.suffix is "_test"
+    assert parsed.chunksize == 50000
+    assert isinstance(parsed.chunksize, int)
+    assert parsed.replace is True
+    assert parsed.verbose is True
+    assert parsed.move is True
+
