69a1cd96bb7f06cbbca3aa477ab63db56e13bc96
Jason Neal <jason.neal@astro.up.pt>
2017-12-05 12:02:11 +0000

    Add continuum subtraction to xcorr_peaks

--- mingle/utilities/xcorr.py
+++ mingle/utilities/xcorr.py
@@ -3,9 +3,29 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from spectrum_overload import Spectrum
+import scipy
+
+def subtractContinuum(s, n=3, plot=False):
+	'''Take a 1D array, use spline to subtract off continuum.
+			subtractContinuum(s, n=3)
+			required:
+			s = the array
+			optional:
+			n = 3, the number of spline points to use
+	'''
+
+	x = np.arange(len(s))
+	points = (np.arange(n)+1)*len(s)/(n+1)
+	spline = scipy.interpolate.LSQUnivariateSpline(x,s,points)
+	if plot:
+		plt.ion()
+		plt.figure()
+		plt.plot(x, s)
+		plt.plot(x, spline(x), linewidth=5, alpha=0.5)
+	return s - spline(x)
 
 
-def xcorr_peak(spectrum, model, plot=False):
+def xcorr_peak(spectrum, model, plot=False, subtract_continuum=True):
     """Find RV offset between a spectrum and a model using PyAstronomy.
 
     Parameters
@@ -25,9 +45,14 @@ def xcorr_peak(spectrum, model, plot=False):
     """
     assert isinstance(spectrum, Spectrum)
     assert isinstance(model, Spectrum)
+    if subtract_continuum:
+        spectrum = spectrum.copy()
+        model = model.copy()
+        spectrum.flux = subtractContinuum(spectrum.flux, n=3, plot=plot)
+        model.flux = subtractContinuum(model.flux, n=3, plot=plot)
 
     rv, cc = spectrum.crosscorr_rv(model, rvmin=-60., rvmax=60.0, drv=0.1,
-                                  mode='doppler', skipedge=50)  # Specturm method
+                                  mode='doppler', skipedge=50)  # Spectrum method
 
     maxind = np.argmax(cc)
     rv_max, cc_max = rv[maxind], cc[maxind]
