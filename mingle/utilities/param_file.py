ad91484c784c94a21695fb3991f8e37f5c95101e
Jason Neal <jason.neal@astro.up.pt>
2017-12-15 13:43:11 +0000

    update paramfile parser

--- mingle/utilities/param_file.py
+++ mingle/utilities/param_file.py
@@ -7,6 +7,7 @@ import simulators
 
 
 def parse_paramfile(param_file: str, path: str = None) -> Dict[str, Union[str, float, List[float]]]:
+    # type: (str, str) -> Dict[str, Union[str, float]]
     """Extract orbit and stellar parameters from parameter file.
 
     Parameters
@@ -23,13 +24,13 @@ def parse_paramfile(param_file: str, path: str = None) -> Dict[str, Union[str, f
     """
     if path is not None:
         param_file = os.path.join(path, param_file)
-    parameters = dict()
+    parameters = dict()  # type: Dict[str, Union[str, float]]
     if not os.path.exists(param_file):
         raise Exception("Invalid Arguments, expected a file that exists not. {0}".format(param_file))
 
     with open(param_file, 'r') as f:
         for line in f:
-            if line.startswith("#"):
+            if line.startswith("#") or line.isspace() or not line:
                 pass
             else:
                 if '#' in line:  # Remove comment from end of line
@@ -74,3 +75,39 @@ def load_paramfile(star):
     param_file = "{0}_params.dat".format(star)
 
     return parse_paramfile(param_file, simulators.paths["parameters"])
+
+
+def parse_obslist(fname, path=None):
+    # type: (str, str) -> List[str]
+    """Parse Obslist file containing list of dates/times.
+
+    Parameters
+    ----------
+    fname: str
+        Filename of obs_list file.
+    path: str [optional]
+        Path to directory of filename.
+
+    Returns
+    --------
+    times: list of strings
+        Observation times in a list.
+    """
+    if path is not None:
+        fname = os.path.join(path, fname)
+    if not os.path.exists(fname):
+        logging.warning("Obs_list file given does not exist. {}".format(fname))
+
+    obstimes = list()
+    with open(fname, 'r') as f:
+        for line in f:
+            if line.startswith("#") or line.isspace() or not line:    # Ignores comments and blank/empty lines.
+                continue
+            else:
+                if "#" in line:   # Remove comment from end of line
+                    line = line.split("#")[0]
+                if "." in line:
+                    line = line.split(".")[0]   # remove fractions of seconds.
+                obstimes.append(line.strip())
+        logging.debug("obstimes = {}",format(obstimes))
+    return obstimes
