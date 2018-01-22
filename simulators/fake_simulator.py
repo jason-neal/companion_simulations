58c04b3585a5e7041e2741582e6cebcf5df68f8f
Jason Neal <jason.neal@astro.up.pt>
2018-01-22 10:51:25 +0000

    Add zero noise option.  SNR=0 or std=0  adds no noise.

--- simulators/fake_simulator.py
+++ simulators/fake_simulator.py
@@ -87,9 +87,12 @@ def fake_iam_simulation(wav, params1, params2, gamma, rv, limits=[2070, 2180],
     iam_grid_models = iam_grid_models / iam_grid_continuum
 
     # This noise is added after continuum normalization.
-    if noise is not None:
+    if noise is not None or noise is not 0:
         # Add 1 / snr noise to continuum normalized spectra
         iam_grid_models = add_noise(iam_grid_models, noise, use_mu=False)
+    else:
+        logging.warning("\n!!!!!!!!!!\n\nNot adding any noise to fake simulation!!!!!!!!!\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
+        print("\n!!!!!!!!!!\n\nNot adding any noise to fake simulation!!!!!!!!!\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
 
     if header:
         return wav, iam_grid_models.squeeze(), mod1_spec.header
