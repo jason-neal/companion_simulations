39b41dd1f87c8f3cc68e4788fdb39851fb40fed6
Jason Neal <jason.neal@astro.up.pt>
2018-01-31 14:33:22 +0000

    Make tmpdir path a str()

--- tests/bin/test_fake_full_stack.py
+++ tests/bin/test_fake_full_stack.py
@@ -5,7 +5,7 @@ from mingle.utilities.param_file import parse_paramfile
 def test_make_fake_parameter_file_creates_file(tmpdir, sim_config):
     """Test fake parameter file creates a file."""
     simulators = sim_config
-    simulators.paths["parameters"] = tmpdir
+    simulators.paths["parameters"] = str(tmpdir)
     info = {"star": "teststar", "temp": 9000, "logg": 6, "feh": 0}
     created_file = tmpdir.join("{}_params.dat".format(info["star"].upper()))
     # Check doesn't exist yet
@@ -18,7 +18,7 @@ def test_make_fake_parameter_file_creates_file(tmpdir, sim_config):
 def test_make_fake_parameter_file_puts_values_in_file(tmpdir, sim_config):
     """Test fake parameter file is the same when read back in."""
     simulators = sim_config
-    simulators.paths["parameters"] = tmpdir
+    simulators.paths["parameters"] = str(tmpdir)
     info = {"star": "teststar", "temp": 9001, "logg": 6,
             "feh": 0.5, "comp_logg": 4, "comp_fe_h": 5}
     created_file = tmpdir.join("{}_params.dat".format(info["star"].upper()))
