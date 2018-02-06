f18ed7fc025a9066f3cf5d0d3f4f28c686337c46
Jason Neal <jason.neal@astro.up.pt>
2018-02-06 22:49:28 +0000

    Add SimReader in db_utils

--- mingle/utilities/db_utils.py
+++ mingle/utilities/db_utils.py
@@ -88,3 +88,71 @@ class DBExtractor(object):
         import warnings
         warnings.warn("Loading in a database may cause memory cap issues.")
 
+
+class SingleSimReader(object):
+    def __init__(self, base=".",
+                 name="BSBHMNOISE",
+                 prefix="", mode="bhm", chi2_val="coadd_chi2"):
+        self.base = base
+        self.name = name.upper()
+        self.prefix = prefix.upper()
+        if mode in ["iam", "tcm", "bhm"]:
+            self.mode = mode
+        else:
+            raise ValueError("Invalid SimReader mode")
+        if chi2_val in ["chi2_1", "chi2_2", "chi2_3", "chi2_4", "coadd_chi2"]:
+            self.chi2_val = chi2_val
+        else:
+            raise ValueError("Invalid chi2_val.")
+
+    def list_sims(self):
+        return glob.glob(os.path.join(self.base, "*"))
+
+    def load_df(self, params=["teff_1", "teff_2", "logg_1", "feh_1"]):
+        params.append(self.chi2_val)
+
+        table = self.get_table()
+        # print(table.c)
+        params = [table.c[p] for p in params]
+        dbdf = pd.read_sql(sa.select(params).order_by(table.c[self.chi2_val].asc()), table.metadata.bind)
+
+        # Coerce to be numeric columns
+        c = dbdf.columns[dbdf.dtypes.eq(object)]
+        dbdf[c] = dbdf[c].apply(pd.to_numeric, errors='coerce', axis=0)
+        return dbdf
+
+    def get_table(self):
+        starname = self.name
+        directory = os.path.join(self.base, starname, self.mode)
+        # print(directory)
+        dbs = glob.glob(os.path.join(directory, "*_coadd_{}_chisqr_results.db".format(self.mode)))
+        # print(dbs)
+        assert len(dbs) == 1, print(len(dbs))
+        dbname = dbs[0]
+        table = load_sql_table(dbname, verbose=False, echo=False)
+        return table
+
+    def params(self):
+        """Get params from param file."""
+        if simulators.paths["parameters"].startswith("."):
+            param_file = os.path.join(self.base, "../", simulators.paths["parameters"],
+                                      "{}_params.dat".format(self.name))
+        else:
+            param_file = os.path.join(simulators.paths["parameters"], "{}_params.dat".format(self.name))
+        params = parse_paramfile(param_file, path=None)
+        print(params)
+        print("self mode", self.mode)
+        if self.mode == "bhm":
+            host_params = target_params(params, mode=self.mode)
+            closest_host_model = closest_model_params(*host_params)  # unpack temp, logg, fe_h with *
+        else:
+            host_params, comp_params = target_params(params, mode=self.mode)
+            closest_host_model = closest_model_params(*host_params)  # unpack temp, logg, fe_h with *
+            closest_comp_model = closest_model_params(*comp_params)
+            params.update(
+                {"teff_2": closest_comp_model[0], "logg_2": closest_comp_model[1], "feh_2": closest_comp_model[2]})
+
+        params.update(
+            {"teff_1": closest_host_model[0], "logg_1": closest_host_model[1], "feh_1": closest_host_model[2]})
+        return params
+
