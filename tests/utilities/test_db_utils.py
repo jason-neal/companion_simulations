40c26b1a643a51559d496e59107007bcbca65cd9
Jason Neal <jason.neal@astro.up.pt>
2018-02-06 23:05:03 +0000

    Fix DBExtactor test,
    Expand faketable for DB test

--- tests/utilities/test_db_utils.py
+++ tests/utilities/test_db_utils.py
@@ -1,20 +1,29 @@
-from mingle.utilities.db_utils import DBExtractor
 import pytest
 
-class FakeTable():
+from mingle.utilities.db_utils import DBExtractor
+
+
+class metadata():
     pass
+
+class FakeTable():
+    def __init__(self):
+        self.metadata = metadata()
+        self.metadata.bind = 5
 
 @pytest.fixture()
 def test_table():
     table = FakeTable()
-    table.c = None
+    table.c = {"label1": 0, "label2": 5}
+    table.metadata.bind = 5
     return table
+
 
 class Test_DBExtractor():
 
-    @pytest.mark.parametrize("limit", [5, 50, 10000])
-    def test_BDExtractor_initalized(self, test_table, limit):
-        x = DBExtractor(table=test_table, limit=limit)
-        assert x.limit == limit
+    def test_BDExtractor_initalized(self, test_table):
+        x = DBExtractor(table=test_table)
         assert isinstance(x, DBExtractor)
+        assert x.table == test_table
+        assert x.cols == test_table.c
 
