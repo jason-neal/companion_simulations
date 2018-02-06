e2bddc26776b8a2ffdbe7cf6a7db3a6a9552df77
Jason Neal <jason.neal@astro.up.pt>
2018-02-06 22:48:48 +0000

    Update DBExtractor class.

--- mingle/utilities/db_utils.py
+++ mingle/utilities/db_utils.py
@@ -4,26 +4,16 @@ import sqlalchemy as sa
 from sqlalchemy import and_
 
 
+
 class DBExtractor(object):
-    """Methods for extracting the relevant code out of database table.
+    """Methods for extracting the relevant code out of database table."""
 
-    """
-
-    def __init__(self, table, limit=10000):
+    def __init__(self, table):
         self.table = table
-        self.limit = limit
-        self.tab_cols = table.c
+        self.cols = table.c
+        self.bind = self.table.metadata.bind
 
-    # def complex_extraction(self, cols, fixed_values, order_value, limit=None):
-    #    if limit is None:
-    #        limit = self.limit
-    #    df = pd.read_sql(
-    #        sa.select([self.tab_cols[par], self.tab_cols[chi2_val]]).where(
-    #            self.tab_cols[par] == float(unique_val)).order_by(
-    #            self.tab_cols[chi2_val].asc()).limit(3), self.table.metadata.bind)
-    #    return df
-
-    def simple_extraction(self, cols, limit=None):
+    def simple_extraction(self, columns, limit=-1):
         """Simple table extraction, cols provided as list
 
         col: list of string
@@ -31,14 +21,12 @@ class DBExtractor(object):
 
         Returns as pandas dataframe.
         """
-        if limit is None:
-            limit = self.limit
-        columns = [self.tab_cols[c] for c in cols]
+        table_columns = [self.cols[c] for c in columns]
         df = pd.read_sql(
-            sa.select(columns).limit(limit), self.table.metadata.bind)
+            sa.select(table_columns).limit(limit), self.bind)
         return df
 
-    def fixed_extraction(self, cols, fixed, limit=None):
+    def fixed_extraction(self, columns, fixed, limit=-1):
         """Table extraction with fixed value contitions.
 
         col: list of string
@@ -49,12 +37,54 @@ class DBExtractor(object):
         Returns as pandas dataframe.
         """
         assert isinstance(fixed, dict)
-        if limit is None:
-            limit = self.limit
-        columns = [self.tab_cols[c] for c in cols]
 
-        conditions = and_(self.tab_cols[key] == value for key, value in fixed.items())
+        table_columns = [self.cols[c] for c in columns]
+
+        conditions = and_(self.cols[key] == value for key, value in fixed.items())
 
         df = pd.read_sql(
-            sa.select(columns).where(conditions).limit(limit), self.table.metadata.bind)
+            sa.select(table_columns).where(conditions).limit(limit), self.bind)
         return df
+
+    def fixed_ordered_extraction(self, columns, fixed, order, limit=-1, asc=True):
+        """Table extraction with fixed value contitions.
+
+        col: list of string
+            Columns to return
+        fixed: dict(key, value)
+        limit: int (optional) default=10000
+
+        Returns as pandas dataframe.
+        """
+        assert isinstance(fixed, dict)
+
+        table_columns = [self.cols[c] for c in columns]
+
+        conditions = and_(self.cols[key] == value for key, value in fixed.items())
+
+        if asc:
+            df = pd.read_sql(
+                sa.select(table_columns).where(conditions).order_by(
+                    self.cols[order].asc()).limit(limit), self.bind)
+        else:
+            df = pd.read_sql(
+                sa.select(table_columns).where(conditions).order_by(
+                    self.cols[order].desc()).limit(limit), self.bind)
+        return df
+
+    def minimum_value_of(self, column):
+        """Return only the entry for the minimum column value, limited to one value.
+        """
+        selection = self.cols[column]
+        df = pd.read_sql(
+            sa.select(self.cols).order_by(selection.asc()).limit(1), self.bind)
+        return df
+
+    def full_extraction(self):
+        return pd.read_sql(
+            sa.select(self.table.c), self.bind)
+
+        """Return Full database:"""
+        import warnings
+        warnings.warn("Loading in a database may cause memory cap issues.")
+
