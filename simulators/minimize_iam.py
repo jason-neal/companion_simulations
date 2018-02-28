09afb6afe9dd4b5f2a99cc62d2960a3f6d51781a
Jason Neal <jason.neal@astro.up.pt>
2018-02-28 17:23:52 +0000

    Create objective functions.

--- simulators/minimize_iam.py
+++ simulators/minimize_iam.py
@@ -8,6 +8,7 @@ from mingle.utilities.errors import betasigma_error
 from mingle.utilities.masking import spectrum_masking
 from mingle.utilities.phoenix_utils import (closest_model_params)
 from mingle.utilities.spectrum_utils import load_spectrum
+from simulators.iam_module import iam_chi2_magic_sauce, iam_magic_sauce
 from simulators.iam_module import (iam_helper_function,
                                    setup_iam_dirs, target_params)
 
@@ -56,6 +57,84 @@ def main(star, obsnum, chip):
     params.add('rv_2', value=0, min=-50, max=50, vary=True)
 
     minimize(iam_chi2, params)
+
+
+def func_chi2(pars, obs_wav, obs_flux, chip=None, norm=True, norm_method="scalar",
+              area_scale=True, wav_scale=True, fudge=None, arb_norm=False, errors=None):
+    """Calculate binary model chi^2 for given parameters and observation"""
+    # unpack parameters: extract .value attribute for each parameter
+    parvals = pars.valuesdict()
+    teff_1 = parvals['teff_1']
+    teff_2 = parvals['teff_2']
+    logg_1 = parvals['logg_1']
+    logg_2 = parvals['logg_2']
+    feh_1 = parvals['feh_1']
+    feh_2 = parvals['feh_2']
+    rv_1 = np.asarray([parvals['rv_1']])
+    rv_2 = np.asarray([parvals['rv_2']])
+
+    chi2_value = iam_chi2_magic_sauce(Spectrum(xaxis=obs_wav, flux=obs_flux),
+                                      [teff_1, logg_1, feh_1],
+                                      [teff_2, logg_2, feh_2],
+                                      rv_1, rv_2,
+                                      chip=chip, norm_method=norm_method,
+                                      area_scale=area_scale, norm=norm,
+                                      wav_scale=wav_scale, fudge=fudge,
+                                      arb_norm=arb_norm, errors=errors)
+    return chi2_value
+
+
+def func_array(pars, obs_wav, obs_flux, errors, chip=None, norm=True, norm_method="scalar",
+               area_scale=True, wav_scale=True, fudge=None, arb_norm=False):
+    """Calculate binary model chi^2 for given parameters and observation"""
+    # unpack parameters: extract .value attribute for each parameter
+    parvals = pars.valuesdict()
+    teff_1 = round(parvals['teff_1'] / 100) * 100
+    teff_2 = round(parvals['teff_2'] / 100) * 100
+    logg_1 = round(parvals['logg_1'] * 2) / 2
+    logg_2 = round(parvals['logg_2'] * 2) / 2
+    feh_1 = round(parvals['feh_1'] * 2) / 2
+    feh_2 = round(parvals['feh_2'] * 2) / 2
+    rv_1 = np.asarray([parvals['rv_1']])
+    rv_2 = np.asarray([parvals['rv_2']])
+
+    obs_flux2, iam_flux = iam_magic_sauce(Spectrum(xaxis=obs_wav, flux=obs_flux),
+                                          [teff_1, logg_1, feh_1],
+                                          [teff_2, logg_2, feh_2],
+                                          rv_1, rv_2,
+                                          chip=chip, norm_method=norm_method,
+                                          area_scale=area_scale, norm=norm,
+                                          wav_scale=wav_scale, fudge=fudge, arb_norm=arb_norm)
+    residual = iam_flux - obs_flux2
+    # Scale to make chi-square sensible.
+    return residual / errors
+
+
+def func_all_chips_array(pars, obs_wav, obs_flux, errors, norm=True, norm_method="scalar",
+                         area_scale=True, wav_scale=True, fudge=None, arb_norm=False):
+    """Calculate binary model chi^2 for given parameters and observation"""
+    # unpack parameters: extract .value attribute for each parameter
+    parvals = pars.valuesdict()
+    teff_1 = round(parvals['teff_1'] / 100) * 100
+    teff_2 = round(parvals['teff_2'] / 100) * 100
+    logg_1 = round(parvals['logg_1'] * 2) / 2
+    logg_2 = round(parvals['logg_2'] * 2) / 2
+    feh_1 = round(parvals['feh_1'] * 2) / 2
+    feh_2 = round(parvals['feh_2'] * 2) / 2
+    rv_1 = np.asarray([parvals['rv_1']])
+    rv_2 = np.asarray([parvals['rv_2']])
+
+    # Detector 1
+    obs_flux2, iam_flux = iam_magic_sauce(Spectrum(xaxis=obs_wav, flux=obs_flux),
+                                          [teff_1, logg_1, feh_1],
+                                          [teff_2, logg_2, feh_2],
+                                          rv_1, rv_2,
+                                          chip=chip, norm_method=norm_method,
+                                          area_scale=area_scale, norm=norm,
+                                          wav_scale=wav_scale, fudge=fudge, arb_norm=arb_norm)
+    residual = iam_flux - obs_flux2
+    # Scale to make chi-square sensible.
+    return residual / errors
 
 
 if __name__ == "__main__":
